6af699a3870828683259ef6d844269dc2cdeca34
Raiga1119 <marvel_0819@icloud.com>
2025-07-09 11:56:42 +0900

    中町　作業途中

--- ProjectVR/Assets/_WorkSpace/Nakamachi/Script/Buttun/StartButtonManager.cs
+++ ProjectVR/Assets/_WorkSpace/Nakamachi/Script/Buttun/StartButtonManager.cs
@@ -1,8 +1,9 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine; //Unityの基本的な機能を使用するための名前空間
+using UnityEngine.SceneManagement; //シーンの管理(切り替えなど)を行うための名前空間
 
 public class StartButtonManager : MonoBehaviour
 {
+    //インスペクター上で
     [SerializeField] private string NextSceneName = "StageSelectScene";
 
     public void OnStartButtonPressed()
